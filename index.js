09d993402de3a7bd99129178c4ddf3d8a796dcae
musaakramsaleh <saleh01715972188@gmail.com>
2024-05-13 12:41:17 +0600

    ok

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors(corsOption))
 app.use(express.json())
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.zuwbcyf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.zuwbcyf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,15 +28,26 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+  const database = client.db("FoodDB");
+    const userCollection = database.collection("food");
+    app.get('/food',async(req,res)=>{
+      const cursor = userCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+
+
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
