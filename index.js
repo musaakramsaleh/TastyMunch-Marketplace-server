b8cec99aebdf1601a0f9b8a9db3c04e3ef97da2b
musaakramsaleh <saleh01715972188@gmail.com>
2024-05-13 15:13:03 +0600

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -35,7 +35,12 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
-
+    app.get('/foods/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.findOne(query)
+      res.send(result)
+    })
 
 
 
