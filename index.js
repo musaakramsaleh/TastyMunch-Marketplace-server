d069c29dfa4c74fa9a60d2ae2416079be74b2b99
musaakramsaleh <saleh01715972188@gmail.com>
2024-05-14 12:22:35 +0600

    ok

--- index.js
+++ index.js
@@ -111,6 +111,10 @@ app.put('/updatefood/:id', async (req, res) => {
       res.json(result);
  
 });
+  app.get('/rankproduct',async(req,res)=>{
+    const result = await userCollection.find().sort({ purchaseCount: -1 }).limit(6).toArray();
+     res.send(result)
+  })
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
